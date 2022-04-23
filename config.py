c732e64aefd3f6706fa1a2d7dfbe762416012120
Ryan Hayabusa <81808867+ryu878@users.noreply.github.com>
2022-04-23 10:26:22 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,32 @@
+key = ''
+secret = ''
+
+symbol = 'XRPBUSD'
+max_size = 160
+asset = 'BUSD'
+
+q0 = 11  # 10
+q1 = 11   # 20
+q2 = 22   # 40
+q3 = 44   # 80
+q4 = 88   # 160
+q5 = 176  # 320
+
+short_tp_static_deduct = 0.0001
+
+max_leverage = 20
+
+atr_mult1 = 3         # 2.5
+atr_mult2 = 7         # 5
+atr_mult3 = 11        # 7.5
+atr_mult4 = 17        # 11
+atr_mult5 = 21        # 15
+
+mult1 = 1
+mult2 = 2
+mult3 = 3
+mult4 = 4
+mult5 = 5
+
+minD_mult = 0.5
+minYG_mult = 0.5
